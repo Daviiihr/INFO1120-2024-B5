85c975af520934527c269dd173105626580cd367
David Fernandez <david.fernandez2024@alu.uct.cl>
2024-06-03 02:33:55 -0400

    Los datos a un DataFrame en pandas

--- contratos.py
+++ contratos.py
@@ -2,9 +2,19 @@ import sqlite3
 import pandas as pd
 
 conn = sqlite3.connect('db_personas.db')
-query = """SELECT * FROM personas"""
+cursor = conn.cursor()
+query = """
+    SELECT * 
+    FROM personas
+    INNER JOIN salarios ON personas.id_rol = salarios.id_salarios
+"""
+cursor.execute(query)
+records = cursor.fetchall()
 
 df = pd.read_sql_query(query, conn)
 conn.close()
+
+columnas = ['Id_rol', 'nombre_completo', 'profesion', 'rut', 'residencia','fecha_ingreso', 'nacionalidad', 'fecha_de_nacimiento', 'id_salarios','Rol', 'Sueldo']
+df = pd.DataFrame(records, columns=columnas)
 
 print(df)
