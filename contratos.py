5c13ca594d6604dd220c30b11fad6cb829ff6dc8
David Fernandez <david.fernandez2024@alu.uct.cl>
2024-06-05 12:40:44 -0400

    los contratos creados se guardan
    los contratos generados para cada persona se guardan en una carpeta especial llamada "contratos_carpetas"

--- contratos.py
+++ contratos.py
@@ -3,6 +3,7 @@ import pandas as pd
 from docx import Document
 import word_gen as wd
 import data as dt
+import os
 
 conn = sqlite3.connect('db_personas.db')
 cursor = conn.cursor()
@@ -29,3 +30,10 @@ personas_dt = personas(df, persona_id)
 print(personas_dt)
 word_contrato = dt.singular_data_to_contract(df, int(persona_id)-1)
 print(word_contrato)
+
+word_contrato = Document()
+nombre_completo = personas_dt['nombre_completo']
+ruta_carpeta = 'contratos_carpeta'
+nombre_archivo = f'{nombre_completo}.docx'
+ruta_completa = os.path.join(ruta_carpeta, nombre_archivo)
+word_contrato.save(ruta_completa)
